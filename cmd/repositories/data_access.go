7a7367ba04f1fb66a92b04186c0b941d106dbd4e
Denis Dvornikov <candyboobers@gmail.com>
2018-02-18 12:39:19 +0300

    cover interface mgo Collection

--- cmd/repositories/data_access.go
+++ cmd/repositories/data_access.go
@@ -1,1 +1,24 @@
 package repositories
+
+import "gopkg.in/mgo.v2"
+
+var (
+	_ DataAccessor = new(DataAccess)
+	_ Querier      = new(mgo.Query)
+)
+
+type DataAccessor interface {
+	Find(interface{}) Querier
+}
+
+type Querier interface {
+	All(interface{}) error
+}
+
+type DataAccess struct {
+	collection *mgo.Collection
+}
+
+func (da *DataAccess) Find(query interface{}) Querier {
+	return da.collection.Find(query)
+}
