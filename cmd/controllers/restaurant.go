792eb9e9cd6aa6909053b4893501413fe950a1ab
Denis Dvornikov <candyboobers@gmail.com>
2018-02-18 16:20:45 +0300

    testing restaurant controller

--- cmd/controllers/restaurant.go
+++ cmd/controllers/restaurant.go
@@ -8,12 +8,20 @@ import (
 )
 
 type RestaurantController struct {
-	Repo repositories.Repo
+	Repo repositories.RestaurantAccessor
 }
 
 // I'm not checking for empty list cause We actually don't wanna see 204,
 // Easier will get empty list and 200
 func (controller *RestaurantController) List(context echo.Context) error {
-	restaurants := controller.Repo.List()
+	restaurants, err := controller.Repo.List()
+	if err != nil {
+		return context.NoContent(http.StatusServiceUnavailable)
+	}
+
 	return context.JSON(http.StatusOK, restaurants)
+}
+
+func NewRestaurantController() *RestaurantController {
+	return &RestaurantController{Repo: repositories.NewRestaurantRepo()}
 }
