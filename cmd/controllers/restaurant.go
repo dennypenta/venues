ba84589d74f399cd6fb07a763627e606e32d1307
Denis Dvornikov <candyboobers@gmail.com>
2018-02-18 12:38:48 +0300

    add validate tags

--- cmd/controllers/restaurant.go
+++ cmd/controllers/restaurant.go
@@ -1,1 +1,19 @@
 package controllers
+
+import (
+	"net/http"
+	"venues/cmd/repositories"
+
+	"github.com/labstack/echo"
+)
+
+type RestaurantController struct {
+	Repo repositories.Repo
+}
+
+// I'm not checking for empty list cause We actually don't wanna see 204,
+// Easier will get empty list and 200
+func (controller *RestaurantController) List(context echo.Context) error {
+	restaurants := controller.Repo.List()
+	return context.JSON(http.StatusOK, restaurants)
+}
