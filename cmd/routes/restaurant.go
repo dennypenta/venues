a70f9f0e067bc27471949bb14426094a743613b4
Denis Dvornikov <candyboobers@gmail.com>
2018-02-18 16:22:25 +0300

    make restaurant route builder

--- cmd/routes/restaurant.go
+++ cmd/routes/restaurant.go
@@ -1,1 +1,12 @@
 package routes
+
+import (
+	"venues/cmd/controllers"
+
+	"github.com/labstack/echo"
+)
+
+func BuildRestaurantGroup(group *echo.Group) {
+	controller := controllers.NewRestaurantController()
+	group.GET("/restaurants", controller.List)
+}
