137462bc4a3bcfbc620d587b1672fb0b8c309a6e
Denis Dvornikov <candyboobers@gmail.com>
2018-02-18 19:31:06 +0300

    move validator into pkg

--- pkg/validator/echo_bind.go
+++ pkg/validator/echo_bind.go
@@ -1,9 +1,20 @@
-package assembly
+package validator
 
 import (
 	"github.com/labstack/echo"
 	"gopkg.in/go-playground/validator.v9"
 )
+func customFunc(fl FieldLevel) bool {
+
+	if fl.Field().String() == "invalid" {
+		return false
+	}
+
+	return true
+}
+
+validate.RegisterValidation("custom tag name", customFunc)
+
 
 type Validator struct {
 	validator *validator.Validate
@@ -13,6 +24,6 @@ func (v *Validator) Validate(i interface{}) error {
 	return v.validator.Struct(i)
 }
 
-func newValidator() echo.Validator {
+func NewValidator() echo.Validator {
 	return &Validator{validator: validator.New()}
 }
